b1b3b9a7a5cc3ad551fa0b246a1584b384b00be5
alkstr <alkstr@mail.ru>
2024-07-18 02:33:29 +0500

    remove periods from format names in the webmedia model

--- VTools/Models/WebMedia.cs
+++ VTools/Models/WebMedia.cs
@@ -6,11 +6,10 @@ namespace VTools.Models
 {
     public partial class WebMedia : ObservableObject
     {
-        public static string[] Formats { get; } = [".mp4", ".webm", ".mp3", ".wav", ".ogg", ".m4a", ".aac", ".flv", ".3gp"];
+        public static string[] Formats { get; } = ["mp4", "webm", "mp3", "wav", "ogg", "m4a", "aac", "flv", "3gp"];
 
         public string URL { get; set; } = "";
         public string Format { get; set; } = Formats.First();
-
 
         [ObservableProperty]
         private Bitmap? thumbnail = null;
