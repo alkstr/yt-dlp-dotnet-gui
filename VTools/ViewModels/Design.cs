c1ecec0f5fad2a54728892d1fbf78bef2cbad59b
alkstr <alkstr@mail.ru>
2024-07-24 14:44:21 +0500

    add viewmodels for design preview

---
+++ VTools/ViewModels/Design.cs
@@ -0,0 +1,29 @@
+﻿using VTools.Utilities;
+
+namespace VTools.ViewModels
+{
+    public class DesignMainViewModel : MainViewModel
+    {
+        public DesignMainViewModel() : base(Configuration.Load())
+        {
+            Config.Culture = Configuration.AvailableCultures[1];
+            Assets.Resources.Culture = Configuration.AvailableCultures[1];
+        }
+    }
+
+    public class DesignDownloaderViewModel : DownloaderViewModel
+    {
+        public DesignDownloaderViewModel()
+        {
+            Assets.Resources.Culture = Configuration.AvailableCultures[1];
+        }
+    }
+
+    public class DesignEditorViewModel : EditorViewModel
+    {
+        public DesignEditorViewModel()
+        {
+            Assets.Resources.Culture = Configuration.AvailableCultures[1];
+        }
+    }
+}
