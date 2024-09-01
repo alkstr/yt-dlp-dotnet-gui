bda21193488f5e0cfa4f63b83044ded23b7bc103
alkstr <alkstr@mail.ru>
2024-09-01 07:26:25 +0500

    refactor Settings tab

--- VTools/Tabs/SettingsTab.axaml.cs
+++ VTools/Tabs/SettingsTab.axaml.cs
@@ -1,86 +1,79 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Threading.Tasks;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Platform.Storage;
+using VTools.Utilities;
 using VTools.ViewModels;
 
 namespace VTools.Tabs;
 
 public partial class SettingsTab : Tab<SettingsViewModel>
 {
-    public SettingsTab() : base(new SettingsViewModel())
-    {
-        InitializeComponent();
-    }
+    public SettingsTab() : base(new SettingsViewModel()) => InitializeComponent();
 
-    public async void ChangeDownloadPathAsync(object sender, RoutedEventArgs args)
-    {
-        var topLevel = TopLevel.GetTopLevel(this)!;
-        var folders = await topLevel.StorageProvider.OpenFolderPickerAsync(new FolderPickerOpenOptions()
-        {
-            AllowMultiple = false
-        });
+    public async void ChangeDownloadFolderAsync(object sender, RoutedEventArgs args) => 
+        await ChangePathAsync(nameof(ViewModel.DownloadPath), StorageItem.Folder);
 
-        if (folders != null && folders.Count > 0 && !string.IsNullOrWhiteSpace(folders[0].Path.ToString()))
-        {
-            ViewModel.DownloadPath = folders[0].TryGetLocalPath()!;
-        }
-    }
+    public async void ChangeYTDLPPathAsync(object sender, RoutedEventArgs args) =>
+        await ChangePathAsync(nameof(ViewModel.YTDLPPath), StorageItem.File);
 
-    public async void ChangeYTDLPPathAsync(object sender, RoutedEventArgs args)
-    {
-        var topLevel = TopLevel.GetTopLevel(this)!;
-        var files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions()
-        {
-            AllowMultiple = false,
-        });
+    public async void ChangeFFmpegPathAsync(object sender, RoutedEventArgs args) =>
+        await ChangePathAsync(nameof(ViewModel.FFmpegPath), StorageItem.File);
 
-        if (files != null && files.Count > 0 && !string.IsNullOrWhiteSpace(files[0].Path.ToString()))
-        {
-            ViewModel.YTDLPPath = files[0].TryGetLocalPath()!;
-        }
-    }
+    public async void ChangeFFprobePathAsync(object sender, RoutedEventArgs args) =>
+        await ChangePathAsync(nameof(ViewModel.FFprobePath), StorageItem.File);
 
-    public async void ChangeFFmpegPathAsync(object sender, RoutedEventArgs args)
-    {
-        var topLevel = TopLevel.GetTopLevel(this)!;
-        var files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions()
-        {
-            AllowMultiple = false,
-        });
-
-        if (files != null && files.Count > 0 && !string.IsNullOrWhiteSpace(files[0].Path.ToString()))
-        {
-            ViewModel.FFmpegPath = files[0].TryGetLocalPath()!;
-        }
-    }
-
-    public async void ChangeFFprobePathAsync(object sender, RoutedEventArgs args)
-    {
-        var topLevel = TopLevel.GetTopLevel(this)!;
-        var files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions()
-        {
-            AllowMultiple = false,
-        });
-
-        if (files != null && files.Count > 0 && !string.IsNullOrWhiteSpace(files[0].Path.ToString()))
-        {
-            ViewModel.FFprobePath = files[0].TryGetLocalPath()!;
-        }
-    }
-
-    public void OpenYTDLPRepositoryURL(object sender, RoutedEventArgs args) =>
+    public void OpenYTDLPDownloadURL(object sender, RoutedEventArgs args) =>
         OpenPath("https://github.com/yt-dlp/yt-dlp/releases/");
 
-    public void OpenFFmpegRepositoryURL(object sender, RoutedEventArgs args) =>
+    public void OpenFFmpegDownloadURL(object sender, RoutedEventArgs args) =>
         OpenPath("https://github.com/BtbN/FFmpeg-Builds/releases");
 
-    public void OpenAppDirectory(object sender, RoutedEventArgs args) =>
+    public void OpenAppFolder(object sender, RoutedEventArgs args) =>
         OpenPath(Directory.GetCurrentDirectory());
 
-    public void OpenRepositoryURL(object sender, RoutedEventArgs args) =>
+    public void OpenAppRepositoryURL(object sender, RoutedEventArgs args) =>
         OpenPath("https://github.com/alkstr/VTools");
+
+    private enum StorageItem
+        {
+        File,
+        Folder,
+        }
+
+    private async Task ChangePathAsync(string pathPropertyName, StorageItem storageItemType)
+    {
+        var topLevel = TopLevel.GetTopLevel(this);
+        if (topLevel == null) { return; }
+
+        IReadOnlyList<IStorageItem> storageItems = [];
+        switch (storageItemType)
+        {
+            case StorageItem.File:
+                storageItems = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions()
+    {
+            AllowMultiple = false,
+        });
+                break;
+            case StorageItem.Folder:
+                storageItems = await topLevel.StorageProvider.OpenFolderPickerAsync(new FolderPickerOpenOptions()
+        {
+            AllowMultiple = false,
+        });
+                break;
+        }
+
+        if (storageItems.Count > 0 && !string.IsNullOrWhiteSpace(storageItems[0].Path.ToString()))
+        {
+            ViewModel
+                .GetType()
+                .GetProperty(pathPropertyName)?
+                .SetValue(ViewModel, storageItems[0].TryGetLocalPath()!);
+        }
+    }
 
     private static void OpenPath(string path)
     {
