5a061e13cdae62a0a9ab1465e419dab58a320c35
alkstr <alkstr@mail.ru>
2024-09-08 14:35:01 +0500

    add scale argument to EditProcess

--- VTools/Utilities/FFmpeg.cs
+++ VTools/Utilities/FFmpeg.cs
@@ -10,14 +10,26 @@ namespace VTools.Utilities
             string ffmpegPath,
             string path,
             (string?, string?) cutInterval,
+            (uint?, uint?) scale,
             string editedFileName,
-            string editedFileExtension) => new()
+            string editedFileExtension)
+        {
+            var scaleArg = scale switch
+            {
+                (null, null) => null,
+                (uint, null) => $"-vf \"scale={scale.Item1}:-1\"",
+                (null, uint) => $"-vf \"scale=-1:{scale.Item2}\"",
+                (uint, uint) => $"-vf \"scale={scale.Item1}:{scale.Item2}",
+            };
+
+            return new Process()
             {
                 StartInfo = new ProcessStartInfo()
                 {
                     FileName = ffmpegPath,
                     Arguments = JoinArguments(
                         $"-i \"{path}\"",
+                        scaleArg,
                         cutInterval.Item1 == null ? null : $"-ss \"{cutInterval.Item1}\"",
                         cutInterval.Item2 == null ? null : $"-to \"{cutInterval.Item2}\"",
                         $"\"{editedFileName}{editedFileExtension}\"",
@@ -29,6 +41,7 @@ namespace VTools.Utilities
                     CreateNoWindow = true,
                 }
             };
+        }
 
         public static Process DurationProcess(
             string ffprobePath,
