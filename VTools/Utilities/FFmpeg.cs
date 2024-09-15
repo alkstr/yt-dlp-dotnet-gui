dd24dfe802fff175360ea32aced03f8fd7b75d54
alkstr <alkstr@mail.ru>
2024-09-15 14:24:11 +0500

    add metadata extraction using FFprobe

--- VTools/Utilities/FFmpeg.cs
+++ VTools/Utilities/FFmpeg.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
@@ -6,6 +7,80 @@ namespace VTools.Utilities
 {
     public static class FFmpeg
     {
+        public static class Metadata
+        {
+            public enum StreamEntry
+            {
+                Duration,
+                Width,
+                Height,
+            }
+
+            public enum FormatEntry
+            {
+                Duration,
+            }
+
+            public static Process Process(
+                string ffprobePath,
+                string filePath,
+                IEnumerable<StreamEntry> streamMetadata,
+                IEnumerable<FormatEntry> formatMetadata)
+            {
+                var streamEntriesStr = string.Join(',', streamMetadata.Select(entry => entry switch
+                {
+                    StreamEntry.Duration => "duration",
+                    StreamEntry.Width => "width",
+                    StreamEntry.Height => "height",
+                    _ => throw new System.NotImplementedException(),
+                }));
+                var formatEntriesStr = string.Join(',', formatMetadata.Select(entry => entry switch
+                {
+                    FormatEntry.Duration => "duration",
+                    _ => throw new System.NotImplementedException(),
+                }));
+
+                var entriesArg = $"-show_entries format=\"{formatEntriesStr}\":stream=\"{streamEntriesStr}\"";
+
+                return new()
+                {
+                    StartInfo = new ProcessStartInfo()
+                    {
+                        FileName = ffprobePath,
+                        Arguments = JoinArguments(
+                            entriesArg,
+                            "-select_streams v:0",
+                            "-v error",
+                            "-of json",
+                            "-sexagesimal",
+                            $"\"{filePath}\""),
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        StandardOutputEncoding = Encoding.UTF8,
+                        StandardErrorEncoding = Encoding.UTF8,
+                        CreateNoWindow = true,
+                    }
+                };
+            }
+
+            public class Output
+            {
+                public class StreamInfo
+                {
+                    public int Width { get; set; }
+                    public int Height { get; set; }
+                }
+
+                public class FormatInfo
+                {
+                    public required string Duration { get; set; }
+                }
+
+                public required IEnumerable<StreamInfo> Streams { get; set; }
+                public required FormatInfo Format { get; set; }
+            }
+        }
+
         public static Process EditProcess(
             string ffmpegPath,
             string path,
--- VTools/ViewModels/EditorViewModel.cs
+++ VTools/ViewModels/EditorViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System.Diagnostics;
 using System.IO;
+using System.Linq;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using Avalonia.Threading;
@@ -22,6 +24,14 @@ public partial class EditorViewModel : ViewModelBase
     }
 
     public enum DurationResult
+    {
+        Success,
+        NoFFprobeError,
+        NoFileError,
+        InvalidOutputError,
+    }
+
+    public enum MetadataResult
     {
         Success,
         NoFFprobeError,
@@ -81,7 +91,49 @@ public partial class EditorViewModel : ViewModelBase
         return DurationResult.Success;
     }
 
+    public async Task<MetadataResult> MetadataAsync()
+    {
+        if (!File.Exists(Configuration.FFprobePath)) { return MetadataResult.NoFFprobeError; }
+        if (!File.Exists(Media.Path)) { return MetadataResult.NoFileError; }
+
+        var process = FFmpeg.Metadata.Process(
+            Configuration.FFprobePath,
+            Media.Path,
+            [FFmpeg.Metadata.StreamEntry.Width, FFmpeg.Metadata.StreamEntry.Height],
+            [FFmpeg.Metadata.FormatEntry.Duration]);
+        process.Start();
+        process.BeginErrorReadLine();
+        process.ErrorDataReceived += OnLogReceived;
+
+        var output = await process.StandardOutput.ReadToEndAsync();
+        var metadata = JsonSerializer.Deserialize<FFmpeg.Metadata.Output>(output, serializerOptions);
+        if (metadata == null) { return MetadataResult.InvalidOutputError; }
+
+        var duration = metadata.Format.Duration.Split(':');
+        Media.Duration = new MediaTime()
+        {
+            Hours = uint.Parse(duration[0]),
+            Minutes = uint.Parse(duration[1]),
+            Seconds = (uint)float.Parse(duration[2])
+        };
+
+        if (metadata.Streams.Any())
+        {
+            Media.Width = metadata.Streams.First().Width;
+            Media.Height = metadata.Streams.First().Height;
+        }
+        else
+        {
+            Media.Width = null;
+            Media.Height = null;
+        }
+
+        await process.WaitForExitAsync();
+        return MetadataResult.Success;
+    }
+
     private readonly object editLock = new();
+    private readonly JsonSerializerOptions serializerOptions = new() { PropertyNameCaseInsensitive = true };
 
     private void OnLogReceived(object sender, DataReceivedEventArgs e)
     {
