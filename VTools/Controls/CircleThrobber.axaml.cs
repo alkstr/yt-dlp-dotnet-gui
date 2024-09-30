69fb19ef5f793b0f64e5bdaeef922a4072ae253e
alkstr <alkstr@mail.ru>
2024-10-01 02:56:43 +0500

    refactor `CircleThrobber`

--- VTools/Controls/CircleThrobber.axaml.cs
+++ VTools/Controls/CircleThrobber.axaml.cs
@@ -2,6 +2,7 @@ using Avalonia.Controls;
 
 namespace VTools.Controls;
 
-public partial class CircleThrobber : UserControl {
+public partial class CircleThrobber : UserControl
+{
     public CircleThrobber() => InitializeComponent();
 }
