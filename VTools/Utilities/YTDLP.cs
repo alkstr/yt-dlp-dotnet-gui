065ceb6f704835001e1bab15e039f04e9546079d
alkstr <alkstr@mail.ru>
2024-10-09 00:59:00 +0500

    refactor `yt-dlp` wrapper

--- VTools/Utilities/YTDLP.cs
+++ VTools/Utilities/YTDLP.cs
@@ -7,95 +7,118 @@ namespace VTools.Utilities
 {
     public static class YTDLP
     {
-        public enum Subtitles
+        public static class Metadata
         {
-            None,
-            Embedded,
-            File,
-        }
-
-        public enum Format
-        {
-            Best,
-            BestAudioOnly,
-        }
-
-        public class MetadataInfo
-        {
-            public required string ExecutablePath;
-            public required string URL;
-            public required IEnumerable<string> Fields;
-        }
-
-        public static Process DownloadProcess(
-            string ytdlpPath,
-            string ffmpegPath,
-            string url,
-            Format format,
-            Subtitles subtitles,
-            string downloadPath)
-        {
-            var formatArg = format switch
+            public enum Field
             {
-                Format.Best => null,
-                Format.BestAudioOnly => "-f bestaudio",
-                _ => throw new System.NotImplementedException(),
-            };
+                Title,
+                Channel,
+                ThumbnailURL,
+            }
 
-            var subtitlesArg = subtitles switch
+            public static Process Process(
+                string ytdlpPath,
+                string url,
+                IEnumerable<Field> fields)
             {
-                Subtitles.None => null,
-                Subtitles.Embedded => "--embed-subs",
-                Subtitles.File => "--write-subs",
-                _ => throw new System.NotImplementedException()
-            };
+                var fieldsArg = string.Join(',', fields.Select(field => field switch
+                {
+                    Field.Title        => "title",
+                    Field.Channel      => "channel",
+                    Field.ThumbnailURL => "thumbnail",
+                    _                  => null,
+                }).Where(f => f != null));
 
-            return new Process()
+                return new()
+                {
+                    StartInfo = new ProcessStartInfo()
+                    {
+                        FileName = ytdlpPath,
+                        Arguments = JoinArguments(url, "-O", fieldsArg, "--encoding utf-8"),
+                        RedirectStandardOutput = true,
+                        RedirectStandardError  = true,
+                        StandardOutputEncoding = Encoding.UTF8,
+                        StandardErrorEncoding  = Encoding.UTF8,
+                        CreateNoWindow = true,
+                    }
+                };
+            }
+        }
+
+        public static class Download
+        {
+            public enum Format
+            {
+                Best,
+                BestAudioOnly,
+            }
+
+            public enum Subtitles
+            {
+                None,
+                Embedded,
+                File,
+            }
+
+            public static Process Process(
+                string ytdlpPath,
+                string ffmpegPath,
+                string url,
+                Format format,
+                Subtitles subtitles,
+                string downloadPath)
+            {
+                var formatArg = format switch
+                {
+                    Format.Best          => null,
+                    Format.BestAudioOnly => "-f bestaudio",
+                    _                    => null,
+                };
+
+                var subtitlesArg = subtitles switch
+                {
+                    Subtitles.None     => null,
+                    Subtitles.Embedded => "--embed-subs",
+                    Subtitles.File     => "--write-subs",
+                    _                  => null,
+                };
+
+                return new Process()
+                {
+                    StartInfo = new ProcessStartInfo()
+                    {
+                        FileName = ytdlpPath,
+                        Arguments = JoinArguments(
+                            $"-P \"{downloadPath}\"",
+                            formatArg,
+                            subtitlesArg,
+                            "-o \"%(title)s [%(id)s].%(ext)s\"",
+                            $"--ffmpeg-location \"{ffmpegPath}\"",
+                            "--extractor-args \"youtube:player_client=mediaconnect\"",
+                            "--encoding utf-8",
+                            $"\"{url}\""),
+                        RedirectStandardOutput = true,
+                        RedirectStandardError  = true,
+                        StandardOutputEncoding = Encoding.UTF8,
+                        StandardErrorEncoding  = Encoding.UTF8,
+                        CreateNoWindow = true,
+                    }
+                };
+            }
+        }
+
+        public static class Update
+        {
+            public static Process Process(string ytdlpPath) => new()
             {
                 StartInfo = new ProcessStartInfo()
                 {
                     FileName = ytdlpPath,
-                    Arguments = JoinArguments(
-                        $"-P \"{downloadPath}\"",
-                        formatArg,
-                        subtitlesArg,
-                        "-o \"%(title)s [%(id)s].%(ext)s\"",
-                        $"--ffmpeg-location \"{ffmpegPath}\"",
-                        "--extractor-args \"youtube:player_client=mediaconnect\"",
-                        "--encoding utf-8",
-                        $"\"{url}\""),
-                    RedirectStandardOutput = true,
-                    RedirectStandardError = true,
-                    StandardOutputEncoding = Encoding.UTF8,
-                    StandardErrorEncoding = Encoding.UTF8,
-                    CreateNoWindow = true,
+                    Arguments = "-U --encoding utf-8",
+                    CreateNoWindow = false,
                 }
             };
         }
-
-        public static Process GetMetadataProcess(MetadataInfo info) => new()
-        {
-            StartInfo = new ProcessStartInfo()
-            {
-                FileName = info.ExecutablePath,
-                Arguments = $"{info.URL} -O {string.Join(',', info.Fields)} --encoding utf-8",
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                StandardOutputEncoding = Encoding.UTF8,
-                StandardErrorEncoding = Encoding.UTF8,
-                CreateNoWindow = true,
-            }
-        };
-
-        public static Process UpdateProcess(string executablePath) => new()
-        {
-            StartInfo = new ProcessStartInfo()
-            {
-                FileName = executablePath,
-                Arguments = $"-U --encoding utf-8",
-                CreateNoWindow = false,
-            }
-        };
 
         private static string JoinArguments(params string?[] arguments) =>
             string.Join(' ', arguments.Where(arg => arg != null));
